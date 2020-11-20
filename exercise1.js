60abfe736249b49a254df901b7cd653f7349c6f9
weilee90 <73676019+weilee90@users.noreply.github.com>
2020-11-19 17:29:44 -0700

    Update exercise1.js

--- exercise1.js
+++ exercise1.js
@@ -1,38 +1,49 @@
 let trans = 1000;
-let ins1,ins2;
+let ins1,ins2,img,vid;
 let ins1IsPlaying = false;
 let ins2IsPlaying = false;
 let extraCanvas;
 let button;
 let compliments;
-let rangeX = 180;
-let rangeY = 206;
-
+let inAnyBoundary = false;
+let visitedboundaries = [];
+let boundaryPoints = [];
+ 
 function preload(){
     ins1 = loadSound('assets/Click.mp3');
     ins2 = loadSound('assets/Twist.mp3');
+    img = loadImage('assets/House.png');
+    //vid = loadVideo('assets/Open.mp4');
 }
 function setup() {
-    createCanvas(400, 400);
+    homeBtn = createButton("Home");
+    homeBtn.mouseClicked(() => {
+        window.open("home.html", "_self");
+    });
+    createCanvas((windowWidth-15), (windowHeight-80));
     button = createButton("Instruction");
     button.mouseClicked(() => {
         buttonhide();
         ins1.play();
     });
-    
+    boundaryPoints.push({x: (width-15)/2-72, y: (height-20)/2+81})
+    boundaryPoints.push({x: (width-15)/2-56, y: (height-20)/2+77})
+    boundaryPoints.push({x: (width-15)/2-42, y: (height-20)/2+72})
+    boundaryPoints.push({x: (width-15)/2-32, y: (height-20)/2+64})
+    boundaryPoints.push({x: (width-15)/2-25, y: (height-20)/2+51})
+    boundaryPoints.push({x: (width-15)/2-18, y: (height-20)/2+37})
 }
 function draw() {
     drawBackground();
-    let handleX = 123;
-    let handleY = 198;
-    let circleX = 180;
-    let circleY = 206;
-
-    if(mouseIsPressed && inTheRange() == true){ //if mouse pressed in bounds then rotate handle
+    let handleX = (width-15)/2 - 77;
+    let handleY = (height-20)/2 + 28;
+    let circleX = (width-15)/2 - 20;
+    let circleY = (height-20)/2 + 36;
+    let clickRange = dist(mouseX, mouseY, circleX, circleY) < 15;
+ 
+    if(mouseIsPressed && checkAllBoundaries(10)){ //if mouse pressed in bounds then rotate handle
         rotateHandle(handleX, handleY);
         rotateCircle(circleX, circleY);
-        rangeX = circleX;
-        rangeY = circleY;
         ins1.stop();
         if(ins2.isPlaying() == false && !ins2IsPlaying){
             ins2.play();
@@ -49,6 +60,33 @@ function draw() {
         strokeWeight(0.5);
     }
 }
+function checkAllBoundaries(size){
+    for(let i = 0; i < boundaryPoints.length; i++){
+        let boundary = {
+          left: boundaryPoints[i].x - size,
+          right: boundaryPoints[i].x + size,
+          top: boundaryPoints[i].y - size,
+          bottom: boundaryPoints[i].y + size
+        }
+        //rect(boundary.left, boundary.top, size*2); //for testing purposes
+        if(mouseX > boundary.left && mouseX < boundary.right && mouseY > boundary.top && mouseY < boundary.bottom){
+            inAnyBoundary = true;
+            console.log(inAnyBoundary);
+		return true;
+            if (!visitedboundaries.includes(boundaryPoints[i])){
+                visitedboundaries.push(boundaryPoints[i]);
+                console.log("In Bounds");
+            }
+            if (visitedboundaries.length >= boundaryPoints.length){ 
+                visitedboundaries = [];
+            }
+            if(!inAnyBoundary){ 
+                    clear();
+			return false;
+            }    
+        }
+        }    
+  }
 function rotateHandle(posX, posY){
     let angle = Math.atan2(mouseY-posY, mouseX-posX);
     if(angle >= 1.3){
@@ -56,7 +94,7 @@ function rotateHandle(posX, posY){
         buttonhide();
         success();
     }
-    translate(123,198);
+    translate((width-15)/2-77,(height-20)/2+28);
     rotate(angle);
     fill(255,197,4,trans);
     rect(0,0,65,17);
@@ -73,31 +111,22 @@ function rotateCircle(posX, posY){
     strokeWeight(0.5);
 }
 function drawBackground(){
+    //vid.position(0,0);
     stroke(0,0,0,trans);
-    background(220);
-    fill(202,164,114,trans);
-    rect(100,0,200,400);
+    background(222,184,135);
+    image(img,-75,-390);
     fill(255,197,4,trans);
-    rect(110,170,30,80,10);
+    rect((width-15)/2-90,(height-20)/2,30,80,10);
     fill(192,192,192,trans);
-    circle(125,205,30);
+    circle((width-15)/2-75,(height-20)/2+35,30);
 }
 function success(){
+    //vid.play();
     trans = 0;
     compliments = createElement('h2', 'Great Job!');
-    compliments.position(123, 100);
+    compliments.position((width-15)/2-70, (height-20)/2)-100;
     textSize(50);
 }
 function buttonhide(){
     button.hide();
 }
-function inTheRange(){
-    if(mouseX>=123 && mouseX<=188 && mouseY>=198 && mouseY<=273){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-//((mouseX-rangeX)^2 + (mouseY-rangeY)^2 <= 35)
